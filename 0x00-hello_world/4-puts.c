6ffee23b8c90e7c9d2f84dead2830e596923a883
donboab <donboab@gmail.com>
2023-02-19 16:31:29 -0800

    Hello world script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-/* This is my first main program */
+/**
+ * This is my first
+ *
+ * main program
+ * return 0 
+ * */
+
 int main(void){
-	puts("Programming is like building a multilingual puzzle"/n);
+	puts("Programming is like building a multilingual puzzle"\n);
 	return(0);
 }
