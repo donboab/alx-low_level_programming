8e3fbafebcbeae8097623391e2885f25b8783726
donboab <donboab@gmail.com>
2023-02-24 18:31:58 -0800

    more functions nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include"main.h"
 
 /**
- * print_numbers - print 0 - 9
- *             only using _putchar twice
- *
+ * print_numbers - print 0 - 9 only using _putchar twice
  * Return: Always 0 (Success)
 */
 
 void print_numbers(void)
 {
-	int num = 0;
-
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
+	int x;
+	for (x =0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
 	_putchar('\n');
 }
