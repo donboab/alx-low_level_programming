a55fd8ae9a64318a7d95e7c6ce7d0fda19d0d2a0
donboab <donboab@gmail.com>
2023-02-27 16:46:57 -0800

    more functions nested loops

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: The string to get the length of input
- * Return: length of @str.
+ * _strlen - Returns the length of a string
+ * @str: The string to get the length of input
+ * Return: The length of @str.
 */
 size_t _strlen(const char *str)
 {
