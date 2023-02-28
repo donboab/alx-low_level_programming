109e944ee4eea4cd58ddeae2466a03e3019d49aa
donboab <donboab@gmail.com>
2023-02-27 16:13:14 -0800

    more functions nested loops

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
+
 /**
  * reset_to_98 - takes a pointer to an int parameter
- * 		and updates the value it points to 98
+ * and updates the value it points to 98
  *
- * @n: int parameter
+ * @n: An integer input pointer
  *
  * Return: Nothing
  */
