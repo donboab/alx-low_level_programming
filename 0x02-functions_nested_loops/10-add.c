fc650d0f5ccecd67e55ed3e467894fc1e52359d2
donboab <donboab@gmail.com>
2023-03-21 21:00:41 -0700

    Nested loopd

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * add - Adds two values
@@ -8,7 +7,7 @@
  * Return:Integer value
  */
 
-int add(int, int)
+int add(int n1, int n2)
 {
-	return (1 + 2);
+	return (n1 + n2);
 }
