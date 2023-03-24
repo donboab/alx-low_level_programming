c0bf5ca946dcdd86d74b18bc2f2301729ce03e0f
donboab <donboab@gmail.com>
2023-03-24 12:41:37 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,15 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
-		_putchar('\n');
+		putchar('\n');
 	else
 	{
 		for (postn = 1; postn <= n; ++postn)
 		{
 			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
+				putchar(' ');
+			putchar(92); /*is equal to '/' char*/
+			putchar('\n');
 		}
 	}
 }
