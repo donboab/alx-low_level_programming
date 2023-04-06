521fc47664da17fa53e1e2cffb3282f1909d2a42
donboab <donboab@gmail.com>
2023-04-05 17:28:44 -0700

    recursion 0x08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * _puts_recursion - This function will print sting and a new line
+/**
+ * _puts_recursion - this function print string amd a new line
  *
  * @s: The parameter for the function
  *
@@ -10,7 +10,7 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != 0)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
