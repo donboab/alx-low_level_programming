d19b0e1081fdf9c4798da368f8ff99094d87c9e3
donboab <donboab@gmail.com>
2023-04-05 18:19:52 -0700

    recursion 0x08

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - the function print factorial of a give number
+ * @n: is the para
+ *
+ * Return: Always return the factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+
+	if (n == 0)
+		return (1);
+	else
+		return (n + factorial(n - 1));
+}
